e973424b9e2ca22074c5fe0bead81266c29e9fb2
PrakGov <55034062+prak112@users.noreply.github.com>
2021-04-29 16:44:16 +0300

    output as suggested. done.

--- ch13_web-services/xml_data-extract.py
+++ ch13_web-services/xml_data-extract.py
@@ -2,6 +2,8 @@
 Python For Everybody
 
 TASK:
+sample_data = http://py4e-data.dr-chuck.net/comments_42.xml
+expt_data = http://py4e-data.dr-chuck.net/comments_1226900.xml
 Return sum of the /count tag present within the /commment tag of the xml file.
 """
 
@@ -9,16 +11,29 @@ Return sum of the /count tag present within the /commment tag of the xml file.
 import urllib.request
 import xml.etree.ElementTree as ET
 
-# data-location
-sample_url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
-expt_url = 'http://py4e-data.dr-chuck.net/comments_1226900.xml'
 
-# parsing sample_data and counting
-response = urllib.request.urlopen(sample_url).read()
-tree = ET.fromstring(response)
-data = tree.findall('comments/comment')
+try:
+    # data location
+    user_input = input('Enter data location: ')
+    response = urllib.request.urlopen(user_input).read()
+    print('Retrieving data from', user_input)
+    
+    # parsing sample_data
+    tree = ET.fromstring(response)
+    print('Retriving xml tree data...')
+    data = tree.findall('comments/comment')
+    
 
-sum = 0
-for comment in data:
-    sum += int(comment.find('count').text)
-print(sum)
+    # extract tags data
+    sum = 0
+    for comment in data:
+        count = comment.find('count').text
+        sum += int(count)
+            
+    print(f"Retrieved {len(response)} characters")
+    print('Sum of count from all count tags: ',sum)
+
+except:
+    print('Enter a valid url for an xml file.')
+
+
